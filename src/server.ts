c273a1d98ecbd9f73dd6007713abcb06ce8bcbec
Finalcreature <finalnadav@gmail.com>
2023-01-30 15:12:07 +0200

    Added socket

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log('Hi!');
+import { Socket, Server } from "socket.io";
+import express from "express";
+import http from "http";
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
