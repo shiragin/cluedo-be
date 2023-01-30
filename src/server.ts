f84db88ed5fa0264899deed8e7d1aeeb3196f725
Finalcreature <finalnadav@gmail.com>
2023-01-30 15:39:05 +0200

    CORS added

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,16 @@ import express from "express";
 import http from "http";
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 io.on("connection", (socket: Socket) => {
   console.log("a user connected");
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(8080, () => {
+  console.log("listening on *:8080");
 });
