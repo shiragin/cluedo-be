97ceaed2bc60147b34acb5441dbb9c7e6a2df841
odeya2626 <odeyacoh26@gmail.com>
2023-01-30 18:51:51 +0200

    connected to mongo

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { Socket, Server } from "socket.io";
 import express from "express";
 import http from "http";
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -13,6 +15,21 @@ io.on("connection", (socket: Socket) => {
   console.log("a user connected");
 });
 
-server.listen(8080, () => {
-  console.log("listening on *:8080");
-});
+
+async function init() {
+  const connection = await mongoose
+    .set("strictQuery", false)
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "cluedo",
+    });
+  if (connection) {
+    console.log("Connected to DB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on ${process.env.PORT}`);
+    });
+  }
+}
+init();
+
